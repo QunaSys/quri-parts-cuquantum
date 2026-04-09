d2e7437bf8958b4b575313f9f9fa929b5d0b8d39
Yusuke Teranishi <teranishi@qunasys.com>
2026-04-09 17:03:09 +0900

    Give precision to evaluate_state_to_vector

--- quri_parts/cuquantum/custatevec/simulator.py
+++ quri_parts/cuquantum/custatevec/simulator.py
@@ -19,6 +19,11 @@ try:
 except ImportError:
     cuquantum = None
 import numpy as np
+from quri_parts.circuit.transpile import (
+    ParallelDecomposer,
+    PauliDecomposeTranspiler,
+    PauliRotationDecomposeTranspiler,
+)
 from quri_parts.core.state import CircuitQuantumState, QuantumStateVector
 
 from . import PRECISIONS, Precision
@@ -30,51 +35,64 @@ for gate_name in gate_map.keys():
 
 
 def evaluate_state_to_vector(
-    state: Union[CircuitQuantumState, QuantumStateVector]
+    state: Union[CircuitQuantumState, QuantumStateVector],
+    precision: Precision = "complex128",
 ) -> QuantumStateVector:
     if cp is None:
         raise RuntimeError("CuPy is not installed.")
     if cuquantum is None:
         raise RuntimeError("cuQuantum is not installed.")
 
+    if precision == "complex64":
+        cuda_d_type = cuquantum.cudaDataType.CUDA_C_32F
+        cuda_c_type = cuquantum.ComputeType.COMPUTE_32F
+    else:
+        cuda_d_type = cuquantum.cudaDataType.CUDA_C_64F
+        cuda_c_type = cuquantum.ComputeType.COMPUTE_64F
+
     qubit_count = state.qubit_count
 
     if isinstance(state, QuantumStateVector):
-        # The type of `QuantumStateVector.vector` is "npt.NDArray[np.complex128]"
-        sv = np.array(state.vector, dtype=np.complex128)
+        sv = np.array(state.vector, dtype=precision)
     else:
-        sv = np.zeros(2**qubit_count, dtype=np.complex128)
+        sv = np.zeros(2**qubit_count, dtype=precision)
         sv[0] = 1.0
 
-    sv = cp.array(sv)
+    sv = cp.array(sv, dtype=precision)
+    circuit = state.circuit
+
+    transpiler = ParallelDecomposer(
+        [PauliDecomposeTranspiler(), PauliRotationDecomposeTranspiler()]
+    )
+    circuit = transpiler(circuit)
 
     mat_dict = {}
 
     handle = cuquantum.custatevec.create()
-    for g in state.circuit.gates:
+    for g in circuit.gates:
         targets = np.array(g.target_indices, dtype=np.int32)
         controls = np.array(g.control_indices, dtype=np.int32)
         if g.name in gates_to_cache:
             if g.name not in mat_dict:
-                mat = cp.array(gate_array(g), dtype="complex128")
+                mat = cp.array(gate_array(g), dtype=precision)
                 mat_dict[g.name] = mat
             else:
                 mat = mat_dict[g.name]
         else:
-            mat = cp.array(gate_array(g), dtype="complex128")
+            mat = cp.array(gate_array(g), dtype=precision)
         mat_ptr = mat.data.ptr
 
         workspaceSize = cuquantum.custatevec.apply_matrix_get_workspace_size(
             handle,
-            cuquantum.cudaDataType.CUDA_C_64F,
+            cuda_d_type,
             qubit_count,
             mat_ptr,
-            cuquantum.cudaDataType.CUDA_C_64F,
+            cuda_d_type,
             cuquantum.custatevec.MatrixLayout.ROW,
             0,
             len(targets),
             len(controls),
-            cuquantum.ComputeType.COMPUTE_64F,
+            cuda_c_type,
         )
 
         # check the size of external workspace
@@ -88,10 +106,10 @@ def evaluate_state_to_vector(
         cuquantum.custatevec.apply_matrix(
             handle,
             sv.data.ptr,  # type: ignore
-            cuquantum.cudaDataType.CUDA_C_64F,
+            cuda_d_type,
             qubit_count,
             mat_ptr,
-            cuquantum.cudaDataType.CUDA_C_64F,
+            cuda_d_type,
             cuquantum.custatevec.MatrixLayout.ROW,
             0,
             targets.ctypes.data,
@@ -99,7 +117,7 @@ def evaluate_state_to_vector(
             controls.ctypes.data,
             0,
             len(controls),
-            cuquantum.ComputeType.COMPUTE_64F,
+            cuda_c_type,
             workspace_ptr,
             workspaceSize,
         )
